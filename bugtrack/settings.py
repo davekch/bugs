4132927770d2f522622764b3eb97f8e155a415f4
davekch <dave-koch@web.de>
2021-05-27 15:27:38 +0200

    allow markdown in issue bodies

--- bugtrack/settings.py
+++ bugtrack/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'markdownify',
 ]
 
 MIDDLEWARE = [
@@ -125,3 +126,9 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# settings for markdownify
+MARKDOWNIFY_MARKDOWN_EXTENSIONS = ['fenced_code']   # allows code blocks between ``` ```
+import bleach
+MARKDOWNIFY_WHITELIST_TAGS = bleach.sanitizer.ALLOWED_TAGS + ['pre']
